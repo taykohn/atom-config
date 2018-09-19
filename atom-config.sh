c8d037a5a1f253cc411e5b42384e35dbbba45f68
taykohn <taylorbkohn@outlook.com>
2018-09-19 14:53:21 -0400

    add my config, keymap, snippets and packages

---
+++ atom-config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# Info:
+#   author:    Taylor Kohn
+#   file:      atom_setup.sh
+#   created:   9/19/2018
+#   version:   1.0
+# -----------------------------------------------------------------------------
+# Requirements:
+#
+# Description:
+#   Setup my preferred atom editor packages, keymap, snippets and configuration.
+# Usage:
+#   atom_setup.sh
+# -----------------------------------------------------------------------------
+# Script:
